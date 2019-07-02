10519f3c4b83019421d593d76a8bf4cb011fcf79
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-02 12:05:46 +0900

    /mypage/main get요청 완성

--- module/cryptoPassword.js
+++ module/cryptoPassword.js
@@ -1,12 +1,12 @@
+// cryptoPassword.js
 const crypto = require('crypto-promise');
 const cryptoPassword = {};
 
-// 랜덤 함수로 salt 생성
+// 랜덤 함수로 salt 생성 
 cryptoPassword.salt = async () => {
     try {
-        const saltBuffer = await crypto.randomBytes(32);
+        const saltBuffer = await crypto.randomBytes(64);
         const salt = saltBuffer.toString('base64');
-        
         return salt;
     } catch (err) {
         console.log(err);
@@ -16,7 +16,7 @@ cryptoPassword.salt = async () => {
 // 해쉬화된 패스워드 출력
 cryptoPassword.hashedPassword = async (password, salt) => {
     try {
-        const hashedPasswordBuffer = await crypto.pbkdf2(password, salt , 1000, 32, 'SHA512');
+        const hashedPasswordBuffer = await crypto.pbkdf2(password, salt , 159203, 64, 'SHA512');
         const hashedPassword = hashedPasswordBuffer.toString('base64'); 
         return hashedPassword;  
     } catch (err) {
