aa5de799d2d0fca59cb32c74f5ac4cb040fb8ba0
seongjin <w8348@naver.com>
2019-07-03 00:34:57 +0900

    마이페이지 결제확인 완료

--- routes/api/mypage/orderlist.js
+++ routes/api/mypage/orderlist.js
@@ -6,14 +6,6 @@ const statusCode = require('../../../module/response/statusCode');
 const pool = require('../../../config/dbConfig');
 const jwt = require('../../../module/jwt');
 
-let data = {
-    created_at: null,
-    main_img : null,
-    name : null,
-    saled_price : null,
-    count : null,
-    is_package : null
-}
 router.get('/', async (req, res) => {
     try {
         var connection = await pool.getConnection();
@@ -23,15 +15,35 @@ router.get('/', async (req, res) => {
         let query1 = "SELECT orders.created_at,main_img,name,ROUND((1-(sale_ratio*0.01))*price,0) AS saled_price,count,is_package FROM order_items left JOIN orders ON order_items.order_id=orders.order_id left JOIN products ON order_items.product_id= products.product_id WHERE user_id=1";
         let result1 = await connection.query(query1);
         delete result1.meta;
-        let data=result1;
+        let data = result1;
         console.log(data);
 
     } catch {
 
-    }finally{
+    } finally {
         connection.release();
     }
 
+})
+
+router.get('/detail', async (req, res) => {
+    try {
+        var connection = await pool.getConnection();
+        //const { user_id } = req.decoded;
+
+        const { order_item_id } = req.query;
+
+        let query1 = "SELECT orders.created_at,main_img,name,ROUND((1-(sale_ratio*0.01))*price,0) AS saled_price,count,is_package,orders.order_id FROM order_items left JOIN orders ON order_items.order_id=orders.order_id left JOIN products ON order_items.product_id= products.product_id WHERE order_item_id=? AND user_id=1";
+        let result1 = await connection.query(query1, [order_item_id]);
+        delete result1.meta;
+        let data = result1;
+        console.log(data);
+
+    } catch {
+
+    } finally {
+        connection.release();
+    }
 })
 
 module.exports = router;
