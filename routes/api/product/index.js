3d2bc892ead39d677161f660aaec5d3f293a3e78
seongjin <w8348@naver.com>
2019-07-02 17:27:31 +0900

    product 라우팅 코드 작성 (응답 관련 코드는 미완성)

--- routes/api/product/index.js
+++ routes/api/product/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 router.use('/regular', require('./regular'));
 router.use('/package', require('./package'));
+router.use('/custom',require('./custom'));
 
 module.exports = router;
