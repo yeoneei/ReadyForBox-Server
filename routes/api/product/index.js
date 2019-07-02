4618ba149a4602b8fd9ce9a08b15d48f4a7a5919
seongjin <w8348@naver.com>
2019-07-02 12:22:27 +0900

    product/package 라우팅 파일 작성

--- routes/api/product/index.js
+++ routes/api/product/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/regular', require('./regular'));
+router.use('/package', require('./package'));
 
 module.exports = router;
