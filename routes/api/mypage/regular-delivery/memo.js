0ffd568c551d7a992d1fe4c192658454cde35216
seongjin <w8348@naver.com>
2019-07-03 18:29:30 +0900

    /mypage/regular-delivery/change/memo 구현

--- routes/api/mypage/regular-delivery/memo.js
+++ routes/api/mypage/regular-delivery/memo.js
@@ -37,6 +37,8 @@ router.put('/', async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
+    } finally {
+        connection.release();
     }
 })
 module.exports = router;
