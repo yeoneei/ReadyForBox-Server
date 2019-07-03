edfb97af7a7580ba41b4d182a1858c82d953d725
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-03 23:59:26 +0900

    /mypage/regular-delivery/product 라우팅 완성

--- routes/api/mypage/main.js
+++ routes/api/mypage/main.js
@@ -20,7 +20,7 @@ router.get('/', jwt.isLoggedIn, async(req, res) => {
     } catch(err) {
         console.log(err);
         res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
-    }
+    } 
 })
 
 module.exports = router;
