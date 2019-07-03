cbf8bc4632467a1004bd47269ac27818dc5f467b
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-03 14:07:07 +0900

    product/regular 응답 관련 코드 수정

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/test', require('./test'));
 router.use('/product',require('./product'));
 router.use('/mypage', require('./mypage'));
 router.use('/auth', require('./auth'));
