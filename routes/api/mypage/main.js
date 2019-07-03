1b541eb6b1c2cfb3a729629c88be22902a15442b
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-03 19:13:40 +0900

    /mypage/main 라우팅 완성

--- routes/api/mypage/main.js
+++ routes/api/mypage/main.js
@@ -12,7 +12,11 @@ router.get('/', jwt.isLoggedIn, async(req, res) => {
             name,
             email
         };
-        res.status(200).json(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, data));
+        if (!name || !email) {
+            res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        } else {
+            res.status(200).json(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, data));
+        }
     } catch(err) {
         console.log(err);
         res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
