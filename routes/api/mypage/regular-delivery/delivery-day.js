bb2ff11d04ccc9e7af29c01e475aa1881e275b99
seongjin <w8348@naver.com>
2019-07-03 18:43:58 +0900

    /mypage/regular-delivery/change/delivery-day 구현

---
+++ routes/api/mypage/regular-delivery/delivery-day.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const utils = require('../../../../module/response/utils');
+const resMessage = require('../../../../module/response/responseMessage');
+const statusCode = require('../../../../module/response/statusCode');
+const pool = require('../../../../config/dbConfig');
+const jwt = require('../../../../module/jwt');
+
+// 배송 일자 변경
+
+router.put('/', async (req, res) => {
+    try {
+        var connection = await pool.getConnection();
+
+        const { order_item_id, delivery_day } = req.body;
+
+        if (!order_item_id || !delivery_day) {
+            res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        } else {
+
+            let query = "SELECT order_id FROM order_items WHERE order_item_id = ?";
+            let result = await connection.query(query, [order_item_id]);
+
+            if (!result[0]) {
+                res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.WRONG_PARAMS));
+            } else {
+                let query2 = "UPDATE regular_deliveries SET delivery_day = ? WHERE order_id = ?";
+                let result2 = await connection.query(query2, [delivery_day, result[0].order_id]);
+
+                if (!result2[0]) {
+                    res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.WRONG_PARAMS));
+                } else {
+                    res.status(200).json(utils.successTrue(statusCode.OK, resMessage.UPDATE_SUCCESS));
+                }
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        connection.release();
+    }
+})
+module.exports = router;
