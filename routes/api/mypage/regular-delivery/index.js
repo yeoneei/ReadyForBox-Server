cd807bae62a45550f70219e11dad5678eb262b84
seongjin <w8348@naver.com>
2019-07-03 16:26:36 +0900

    마이페이지-정기배송관리-수량감소 구현완료

--- routes/api/mypage/regular-delivery/index.js
+++ routes/api/mypage/regular-delivery/index.js
@@ -4,4 +4,5 @@ var router = express.Router();
 router.use('/product', require('./product'));
 router.use('/change', require('./change'));
 router.use('/change/address', require('./address'));
+router.use('/change/count-minus', require('./count-minus'));
 module.exports = router;
