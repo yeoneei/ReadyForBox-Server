d6937d75e7942749af04f0afa00a5a341ac8eecc
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-08 22:58:09 +0900

    push 전 커밋

--- routes/api/mypage/index.js
+++ routes/api/mypage/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/main', require('./main'));
+router.use('/user', require('./user'));
 router.use('/orderlist', require('./orderlist'));
 router.use('/regular-delivery',require('./regular-delivery'));
 
