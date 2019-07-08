cd6513dda14c7711e893207b2828c1207a0a3b96
seongjin <w8348@naver.com>
2019-07-08 23:06:37 +0900

    /mypage/user 라우팅 완성

--- routes/api/mypage/user.js
+++ routes/api/mypage/user.js
@@ -3,30 +3,42 @@ var router = express.Router();
 const utils = require('../../../module/response/utils');
 const resMessage = require('../../../module/response/responseMessage');
 const statusCode = require('../../../module/response/statusCode');
+const pool = require('../../../config/dbConfig');
 const jwt = require('../../../module/jwt');
 
 
-router.get('/', jwt.isLoggedIn, async(req, res) => {
+router.get('/', jwt.isLoggedIn, async (req, res) => {
     try {
+        var connection = await pool.getConnection();
         const { name, email } = req.decoded;
-        const data = {
-            user_id,
-            name,
-            email,
-            phone,
-            address,
-            birth,
-            gender
-        };
+
         if (!name || !email) {
             res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
         } else {
-            res.status(200).json(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, data));
+            let query = "SELECT * FROM users WHERE name = ? "
+            let result = await connection.query(query, [name]);
+            if (!result) {
+                res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.WRONG_PARAMS));
+            } else {
+                let data = {
+                    user_id: result[0].user_id,
+                    name: name,
+                    email: email,
+                    phone: result[0].phone,
+                    address: (result[0].address1 + result[0].address2 + result[0].address_detail),
+                    birth: result[0].birth,
+                    gender: result[0].gender
+                }
+                console.log(data);
+                res.status(200).json(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, data));
+            }
         }
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
-    } 
+    } finally {
+        connection.release();
+    }
 })
 
 module.exports = router;
