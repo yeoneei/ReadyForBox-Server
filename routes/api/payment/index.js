cc6ad5d8c7ecec1e16e772d5b3829941ac2fda25
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-11 16:57:07 +0900

    테스트

--- routes/api/payment/index.js
+++ routes/api/payment/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 router.use('/billings', require('./billings'));
 router.use('/iamport-callback', require('./iamport-callback'));
 router.use('/', require('./payment'));
+
+// 테스트용
 router.use('/payment_test', require('./payment_test'));
+router.use('/billings_test', require('./billings_test'));
 
 module.exports = router;
