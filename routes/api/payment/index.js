9722bfbc9c9b6824956c1d8da08f753e103de4cc
ForTheOtherss <qkrwotjd1445@naver.com>
2019-07-11 15:46:43 +0900

    에러 수정

--- routes/api/payment/index.js
+++ routes/api/payment/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 router.use('/billings', require('./billings'));
 router.use('/iamport-callback', require('./iamport-callback'));
 router.use('/', require('./payment'));
+router.use('/payment_test', require('./payment_test'));
 
 module.exports = router;
